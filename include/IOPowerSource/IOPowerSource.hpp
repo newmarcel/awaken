87d4c63decfe7371f07c8db6d917e82d524c91f5
Marcel Dierkes <marcel@dierkes.co>
2020-05-07 19:19:55 +0200

    Added IOPowerSource

---
+++ include/IOPowerSource/IOPowerSource.hpp
@@ -0,0 +1,19 @@
+//
+//  IOPowerSource.hpp
+//  awaken
+//
+//  Created by Marcel Dierkes on 07.05.20.
+//  Copyright © 2020 Marcel Dierkes. All rights reserved.
+//
+
+#ifndef IOPowerSource_hpp
+#define IOPowerSource_hpp
+
+namespace Awaken
+{
+
+
+
+}
+
+#endif /* IOPowerSource_hpp */
---
+++ src/IOPowerSource/IOPowerSource.cpp
@@ -0,0 +1,15 @@
+//
+//  IOPowerSource.cpp
+//  awaken
+//
+//  Created by Marcel Dierkes on 07.05.20.
+//  Copyright © 2020 Marcel Dierkes. All rights reserved.
+//
+
+#include "IOPowerSource/IOPowerSource.hpp"
+#include <CoreFoundation/CoreFoundation.h>
+#import <IOKit/ps/IOPowerSources.h>
+#include "Log.hpp"
+
+using namespace std;
+using namespace Awaken;
